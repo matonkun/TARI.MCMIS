d52087ebd1264e3d5ae1d0a9d6f7942d81fb5fc6
Hsiaoplay-PC\Hsiaoplay <hsiaoplay414@gmail.com>
2016-10-27 15:51:51 +0800

    增加登入畫面，選單畫面

--- TariMcmis/source/app/src/main/java/com/ksi/tarimcmis/LoginActivity.java
+++ TariMcmis/source/app/src/main/java/com/ksi/tarimcmis/LoginActivity.java
@@ -1,13 +1,35 @@
 package com.ksi.tarimcmis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button LoginBtn,ForgetBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findID();
+
+        LoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toMenu = new Intent();
+                toMenu.setClass(LoginActivity.this,MenuActivity.class);
+                startActivity(toMenu);
+            }
+        });
+
+    }
+
+    private void findID(){
+        LoginBtn = (Button)findViewById(R.id.loginbtn);
+        ForgetBtn = (Button) findViewById(R.id.forgetbtn);
     }
 }
